37771ad1e550cc3b70f18fe9af31e8533f0b4930
Croak-Croak <azamat.kalmykov07@gmail.com>
2023-09-21 20:18:26 +0300

    add consts

---
+++ src/shared/const/localStorageKeys.js
@@ -0,0 +1,1 @@
+export const TASKLIST = 'TASKLIST';
---
+++ src/shared/const/selectOptions.js
@@ -0,0 +1,25 @@
+export const filterOptions = [
+  {
+    name: 'All',
+    value: 'all',
+  },
+  {
+    name: 'Done',
+    value: 'true',
+  },
+  {
+    name: 'Undone',
+    value: 'false',
+  },
+];
+
+export const sortOptions = [
+  {
+    name: 'New',
+    value: 'asc',
+  },
+  {
+    name: 'Old',
+    value: 'desc',
+  },
+];
